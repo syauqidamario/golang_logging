cf2ef9c1ed61f99fdc22d19cbbfd7d0a2f83bf08
syauqidamario <syauqi.djohan@student.umn.ac.id>
2024-06-01 14:36:21 +0700

    Output in Go-Lang logging

---
+++ output_test.go
@@ -0,0 +1,17 @@
+package golang_logging
+
+import (
+	"os"
+	"testing"
+
+	"github.com/sirupsen/logrus"
+)
+
+func TestOutput(t *testing.T) {
+	logger := logrus.New()
+	file, _ := os.OpenFile("application.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	logger.SetOutput(file)
+	logger.Info("Hello World")
+	logger.Warn("Hello World")
+	logger.Error("Hello World")
+}
